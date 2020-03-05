402603ca2affcb1f786c538535902e9fbde030c6
unknown <shyleshpatil@gmail.com>
2020-03-05 12:13:45 +0000

    Vector

---
+++ VectorExc.R
@@ -0,0 +1,6 @@
+vec1 <- c("Hockey","Football","Baseball","Curling","Rugby","Hurling","basketball","Tennis","Cricket","Lacrosse")
+vec2 <- c(vec1, "Hockey", "Lacrosse", "Hockey", "Waterpolo","Hockey")
+vec3 <- vec2[c(1,3,6)]
+
+print(vec3)
+
